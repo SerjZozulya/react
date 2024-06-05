a335efac8f1679c9b45a74b0ea81391414f55325
Sergey Zozulya <sergey.zozulya@tibbo.com>
2024-06-05 23:37:56 +0300

    раскомментил провайдер

--- src/index.tsx
+++ src/index.tsx
@@ -11,11 +11,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-console.log('index')
+console.log("index");
 root.render(
-//  <Provider store={store}>
-
-      <App />
-
-//  </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
